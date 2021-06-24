8e9e0f3e53a20152ab9ff15fd8bef0fa342e5e32
mayurpat <mayurpat_in@yahoo.com>
2021-06-24 18:09:13 -0500

    Checking samples demo

--- src/test/java/com/example/demo/utility/FizzBuzzUtilityTest.java
+++ src/test/java/com/example/demo/utility/FizzBuzzUtilityTest.java
@@ -1,18 +1,46 @@
 package com.example.demo.utility;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 
 public class FizzBuzzUtilityTest {
-	FizzBuzzUtility fb = new FizzBuzzUtility();
+	
+	@Autowired
+	FizzBuzzUtility fb ;
+	
+	@BeforeEach
+	public void setUp() {
+		fb = new com.example.demo.utility.FizzBuzzUtility();
+		
+	}
+	
 	
 	@Test
 	public void testFizz() {
-		String result ="";
 		assertEquals("FIZZ", fb.checkFizzBuzz(3));
 	}
+	
+	@Test
+	public void testBuzz() {
+		assertEquals("BUZZ", fb.checkFizzBuzz(5));
+	}
+	
+	
+	@Test
+	public void testFizzBuzz() {
+		assertEquals("FIZZBUZZ", fb.checkFizzBuzz(15));
+	}
+	
+	@Test
+	public void testNotFizzBuzz() {
+		assertNotEquals("FIZZBUZZ", fb.checkFizzBuzz(1));
+	}
+	
 
 }
